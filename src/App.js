cd0983df351aa760f5b809436a256ae6899239ca
Javed Chauhan <chauhanjaved@gmail.com>
2023-09-14 16:05:40 +0530

    Update colors array

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const color = [
-    "#673AB7", // Deep Purple
-    "#00796B", // Dark Teal
-    "#800000", // Burgundy
-    "#FF5722", // Dark Orange
-    "#004D40"  // Midnight Green
-  ];  
+  const gemstoneColors = [
+    { name: "Amethyst", color: "#64609A" },
+    { name: "Citrine", color: "#933709" },
+    { name: "Emerald", color: "#14A989" },
+    { name: "Jade", color: "#469A84" },
+    { name: "Jasper", color: "#D05340" },
+    { name: "Lapis Lazuli", color: "#436CB9" },
+    { name: "Malachite", color: "#469496" },
+    { name: "Moonstone", color: "#3AA8C1" },
+    { name: "Onyx", color: "#353839" },
+    { name: "Peridot", color: "#ABAD48" },
+    { name: "Pink Pearl", color: "#B07080" },
+    { name: "Rose Quartz", color: "#BD559C" },
+    { name: "Ruby", color: "#AA4069" },
+    { name: "Sapphire", color: "#2D5DA1" },
+    { name: "Smokey Topaz", color: "#832A0D" },
+    { name: "Tiger's Eye", color: "#B56917" }
+  ];
+  
   const getRandomColor = () =>{
-    return color[Math.floor(Math.random() * color.length)]
+    return gemstoneColors[Math.floor(Math.random() * gemstoneColors.length)]
   }
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
@@ -36,12 +48,12 @@ function App() {
         )}`;
         // Update the anchor element's href here
         document.getElementById("tweet-quote").href = tweetUrl;
-        const color = getRandomColor();
-        document.body.style.backgroundColor = color;
-        document.body.style.color = color;
-        document.getElementById("new-quote").style.backgroundColor = color;
+        const randomColor = getRandomColor();
+        document.body.style.backgroundColor = randomColor.color;
+        document.body.style.color = randomColor.color;
+        document.getElementById("new-quote").style.backgroundColor = randomColor.color;
         document.getElementById("new-quote").style.color = "#FFFFFF";
-        document.getElementById("tweet-quote").style.color = color;
+        document.getElementById("tweet-quote").style.color = randomColor.color;
 
       })
       .catch((error) => {
